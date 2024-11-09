16e25bbaa1c1d7d4888003c80568eab3b4c4b24c
KonstantinKalev <constantinekalev27@itpg-varna.bg>
2024-11-09 19:03:56 +0200

    JS файл, съдържащ анимацията на generating.html

---
+++ scripts/generatingAnimation.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let textElement = document.getElementById('text');
+    let originalText = "Вашият линк се генерира.";
+    let count = 0;
+
+    setInterval(function() {
+        count = (count + 1) % 3;
+        let dots = ".".repeat(count);
+        textElement.innerText = `${originalText}${dots}`;
+    }, 500);
+});
